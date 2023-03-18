a91af84e100274b6c99e4c7d66c80e25ca13fc04
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-03-18 07:06:26 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - main function
@@ -9,9 +9,11 @@
 int main(void)
 {
 	char i;
-	for (i ='a' ; i <  'z' ; i++)
+
+	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
-	for (i = 'a' ; i < = 'Z' ; i++)
+	for (i = 'A' ; i <= 'Z' ; i++)
 		putchar (i);
-	putchar("\");
-	return (0)
+	putchar('\n');
+	return (0);
+}
