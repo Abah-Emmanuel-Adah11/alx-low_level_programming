8cba6e11726c9df78a67a0552f22142eee7e4fa5
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-03-22 15:15:55 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,6 +21,7 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 	}
+
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
@@ -39,6 +40,7 @@ int main(void)
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
+
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
