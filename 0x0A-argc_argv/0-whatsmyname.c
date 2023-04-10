6426a134203de3d541bb5e4f2b4e2e0b1edd7e77
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-04-10 13:47:10 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints its name, followed by a new line
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
