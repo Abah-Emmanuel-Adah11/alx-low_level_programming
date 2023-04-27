328a1a38da573ca3369b1f673a7ee55d05745e40
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-04-27 14:28:44 -0700

    Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
-#include "list.h"
+#include "lists.h"
+
 /**
  * free_list - A function that frees a linked list list_t.
  * @head: list_t list to be freed
- *
- * Return: Number of elements in h
  */
 void free_list(list_t *head)
 {
