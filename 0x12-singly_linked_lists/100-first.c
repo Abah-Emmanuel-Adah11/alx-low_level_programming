f03186c5f7119188a9bbfb0af7120f60ba01cc27
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-04-27 14:40:45 -0700

    Task 1 Advance

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void first(void)__attribue__((constructor));
+
+/**
+ * first - Prints a sentence before the main
+ * function is executed
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house on my back!\n");
+}
