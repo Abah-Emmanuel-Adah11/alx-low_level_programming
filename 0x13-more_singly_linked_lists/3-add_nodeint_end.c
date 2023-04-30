dd477142e0f82d8a11d0e1c8e90302fd011db8bd
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-04-30 07:49:56 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *add;
 	listint_t *temp = *head;
 
-	new = malloc(sizeof(listint_t));
+	add = malloc(sizeof(listint_t));
 	if (!add)
 		return (NULL);
 
