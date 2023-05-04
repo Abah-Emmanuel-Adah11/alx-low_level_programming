54f801358d9321ae4b0a55b2be77a2aba7c634aa
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-05-04 13:54:19 -0700

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -18,7 +18,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	for (i = 63; i >=0; i--)
 	{
 		latest = exclusive >> i;
-		if (current & 1)
+		if (latest & 1)
 			count++;
 	}
 
