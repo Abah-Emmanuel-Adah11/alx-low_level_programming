cff5bb742a67bb392c7dffbd8400e912b9858ac9
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-05-04 14:22:54 -0700

    Task 100

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_enddianness - A function that checks if a machine is little
+ * get_endianness - A function that checks if a machine is little
  * or big endian
  * Return: If big return 0 and 1 if little
  */
