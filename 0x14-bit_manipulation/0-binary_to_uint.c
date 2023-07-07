95d4d8139cf14a6765c59a05b9eec9168d52ba04
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-07-07 07:07:55 -0700

    Task 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - A function that convert a binary number number to an
+ * unsigned int.
+ * @b: string that contains the binary number
+ *
+ * Return: The converted number
+ */
+
+unsigned int binary_to_unit(const char *b)
+{
+	int i;
+	unsigned int deci_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		deci_val = 2 * deci_val + (b[i] - '0');
+	}
+
+	return (deci_val);
+}
