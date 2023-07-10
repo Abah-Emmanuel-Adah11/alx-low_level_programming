0f5577b99ce964170cec1adad89fd539cf8f14c3
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-07-10 05:30:19 -0700

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,14 +10,14 @@
  * otherwise - 1.
  */
 
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w. len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content !=NULL)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
